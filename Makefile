9408d4ae0e33ddd9934d980a0161251295d07670
Damir <damir.doszhan@gmail.com>
2018-05-30 23:56:32 +0600

    Third step of level 1 project

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
-install: 
+install:
 	npm install
 start:
 	npm run babel-node -- src/bin/brain-games.js
 publish:
 	npm run build
 	npm publish
+lint:
+	npm run eslint
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const getUserName = () => {
-  console.log("Welcome to the Brain Games");
-  let name = readlineSync.question('May I have your name? ');
-  console.log("Hello, " + name + "!");
-}
+const getUserName = () => {
+  console.log('Welcome to the Brain Games');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
 
+export { getUserName as default };
