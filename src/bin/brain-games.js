c8e37f7534bad6447c712f21088bd518bb1e760e
Damir <damir.doszhan@gmail.com>
2018-05-30 09:24:14 +0600

    Corrected mentor's comments in step 2

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 import { getUserName } from '..';
-console.log("Welcome to the Brain Games");
 
-console.log("Hello, " + getUserName() + "!");
+getUserName();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const getUserName = () => readlineSync.question('May I have your name? ');
+export const getUserName = () => {
+  console.log("Welcome to the Brain Games");
+  let name = readlineSync.question('May I have your name? ');
+  console.log("Hello, " + name + "!");
+}
 
