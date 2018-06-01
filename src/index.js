f8a3804110ca6959d7f8b34b553e1528efba35fd
Damir <damir.doszhan@gmail.com>
2018-06-01 17:09:31 +0600

    Forth step of 1 level Hexlet project

--- src/index.js
+++ src/index.js
@@ -6,4 +6,36 @@ const getUserName = () => {
   console.log(`Hello, ${name}!`);
 };
 
-export { getUserName as default };
+const brainGames = () => {
+  const FLOOR_OF_RANDOM_NUMBER = 0;
+  const CEIL_OF_RANDOM_NUMBER = 1000;
+  const GAME_ROUND_CEIL = 3;
+
+  const generateRandomNumber = (min, max) => Math.floor(min + (Math.random() * ((max + 1) - min)));
+
+  console.log('Welcome to the Brain Games');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!\n`);
+
+  let round = 1;
+  while (round <= GAME_ROUND_CEIL) {
+    const randomNumber = generateRandomNumber(FLOOR_OF_RANDOM_NUMBER, CEIL_OF_RANDOM_NUMBER);
+    console.log(`Quesion: ${randomNumber}`);
+    const systemAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === systemAnswer) {
+      console.log('Correct');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${systemAnswer}'.`);
+      console.log(`Let's try again, ${userName}!\n`);
+      break;
+    }
+    if (round === GAME_ROUND_CEIL) {
+      console.log(`Congratulations, ${userName}!\n`);
+    }
+    round += 1;
+  }
+};
+
+export { brainGames, getUserName };
