43a8cf31375c937a9eaca214095101864572da93
Damir <damir.doszhan@gmail.com>
2018-06-03 11:42:07 +0600

    Ninth step of 1 level Hexlet project

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,18 +4,18 @@ import generateRandomNumber from '../utils';
 
 const floorOfRandomNumber = 2;
 const ceilOfRandomNumber = 1000;
+const isPrime = (number) => {
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
 
 const gameFunction = () => {
   const randomNumber = generateRandomNumber(floorOfRandomNumber, ceilOfRandomNumber);
   const question = `${randomNumber}`;
-  const isPrime = (number) => {
-    for (let i = 2; i < number; i += 1) {
-      if (number % i === 0) {
-        return 'no';
-      }
-    }
-    return 'yes';
-  };
   const systemAnswer = isPrime(randomNumber);
   return cons(question, systemAnswer);
 };
